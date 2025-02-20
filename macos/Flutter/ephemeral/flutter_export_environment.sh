ffd29886ef4cf9a059f450f1e9d6049664d0d07a
Mayumi Hirata Bogoni <130617303+mayumihb@users.noreply.github.com>
2025-02-20 19:23:28 -0300

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\dev\ADS\semestre4\desenvolvimento_moveis\flutter\password_generator\lib\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\dev\ADS\semestre5\dispositivos-moveis\Aula1_Flutter_2025"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\ADS\\semestre4\\desenvolvimento_moveis\\flutter\\password_generator\\lib\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\dev\\ADS\\semestre5\\dispositivos-moveis\\Aula1_Flutter_2025" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\ADS\\semestre4\\desenvolvimento_moveis\\flutter\\password_generator\\lib\\flutter"
+  "PROJECT_DIR=C:\\dev\\ADS\\semestre5\\dispositivos-moveis\\Aula1_Flutter_2025"
+  "FLUTTER_ROOT=C:\\dev\\ADS\\semestre4\\desenvolvimento_moveis\\flutter\\password_generator\\lib\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\dev\\ADS\\semestre5\\dispositivos-moveis\\Aula1_Flutter_2025\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\dev\\ADS\\semestre5\\dispositivos-moveis\\Aula1_Flutter_2025"
+  "FLUTTER_TARGET=C:\\dev\\ADS\\semestre5\\dispositivos-moveis\\Aula1_Flutter_2025\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\dev\\ADS\\semestre5\\dispositivos-moveis\\Aula1_Flutter_2025\\.dart_tool\\package_config.json"
+)
